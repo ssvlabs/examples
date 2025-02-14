8e96fa992c4aec234451aaf8bf9e8c0e961d1e13
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-02-14 07:08:24 -0300

    Add summary logging to main function

--- simple-block-agreement/operator/src/index.ts
+++ simple-block-agreement/operator/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 
 import { App } from './app/app'
 import { getData } from './data/get-data'
+import { logBAppSummary } from './logging'
 
 dotenv.config()
 
@@ -13,6 +14,9 @@ const main = async () => {
   const useHarmonicCombination = process.env.USE_HARMONIC_COMBINATION_FUNCTION === 'true'
 
   const { bApp, strategies, slot } = await getData(bAppAddress)
+
+  // Logging
+  logBAppSummary(bApp, strategies)
 
   app.Setup(bApp, strategies, useExponentialWeight, useHarmonicCombination)
   app.StartAgreement(slot)
